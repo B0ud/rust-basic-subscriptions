7f6c36cbc2e99618e33b0c2406145d985d9d4b4b
B0ud <mehdi.boudart@gmail.com>
2022-06-23 16:51:45 +0200

    Type Driveen development

--- Vagrantfile
+++ Vagrantfile
@@ -11,10 +11,15 @@ unless Vagrant.has_plugin?("vagrant-docker-compose")
 Vagrant.configure("2") do |config|
     config.vm.box = "ubuntu/jammy64"
     config.vm.synced_folder '.', '/vagrant', disabled: true
-    config.vm.synced_folder ".", "/home/vagrant/app", disabled: false
+    config.vm.synced_folder ".", "/home/vagrant/app", disabled: false, type: "rsync", rsync__auto: true, rsync__exclude: ['.vagrant/', 'target/', '.git/']
 
     config.vm.network(:forwarded_port, guest: 5432, host: 5432)
 
     config.vm.provision :docker
     config.vm.provision :docker_compose, yml: "/home/vagrant/app/docker-compose.yml", run: "always"
+
+    config.vm.provider "virtualbox" do |v|
+      v.memory = 2048
+      v.cpus = 4
+    end
   end
--- src/configuration.rs
+++ src/configuration.rs
@@ -1,50 +1,109 @@
 use secrecy::ExposeSecret;
 use secrecy::Secret;
-#[derive(serde::Deserialize)]
-pub struct Settings {
-    pub database: DatabaseSettings,
-    pub application_port: u16,
-}
-#[derive(serde::Deserialize)]
+use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::PgSslMode;
+use sqlx::ConnectOptions;
+
+#[derive(serde::Deserialize, Debug)]
 pub struct DatabaseSettings {
+    pub require_ssl: bool,
     pub username: String,
     pub password: Secret<String>,
     pub port: u16,
     pub host: String,
     pub database_name: String,
 }
+#[derive(serde::Deserialize, Debug)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application: ApplicationSettings,
+}
 
 impl DatabaseSettings {
-    pub fn connection_string(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port,
-            self.database_name
-        ))
+    pub fn with_db(&self) -> PgConnectOptions {
+        let mut options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing::log::LevelFilter::Trace);
+
+        options
     }
 
-    pub fn connection_string_without_db(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port
-        ))
+    pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            // Try an encrypted connection, fallback to unencrypted if it fails
+            PgSslMode::Prefer
+        };
+
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .port(self.port)
+            .ssl_mode(ssl_mode)
+    }
+}
+
+/// The possible runtime environment for our application.
+pub enum Environment {
+    Local,
+    Production,
+}
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+impl TryFrom<String> for Environment {
+    type Error = String;
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
     }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    // Detect the running environment.
+    // Default to `local` if unspecified.
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+
     // Initialise our configuration reader
     let settings = config::Config::builder()
-        .add_source(config::File::with_name("configuration"))
+        .add_source(config::File::from(configuration_directory.join("base")).required(true))
+        .add_source(
+            config::File::from(configuration_directory.join(environment.as_str())).required(true),
+        )
+        //.add_source(config::File::with_name("configuration"))
+        .add_source(config::Environment::with_prefix("APP").separator("_"))
         .build()
         .unwrap();
 
     // Try to convert the configuration values it read into
     // our Settings type
-    settings.try_deserialize()
+    let config = settings.try_deserialize();
+
+    println!("{:?}", config);
+
+    config
 }
