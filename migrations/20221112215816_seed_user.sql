55c4f06e95a3d49e1c21607b19730a8ed3e85fcc
B0ud <mehdi.boudart@gmail.com>
2023-01-14 17:47:01 +0100

    Change default password

--- migrations/20221112215816_seed_user.sql
+++ migrations/20221112215816_seed_user.sql
@@ -3,5 +3,5 @@ INSERT INTO users (user_id, username, password_hash)
 VALUES (
     'ddf8994f-d522-4659-8d02-c1d479057be6',
     'admin',
-    '$argon2id$v=19$m=15000,t=2,p=1$qzJy6GqdwNlBzifS9dtcQQ$tvngniRSUF/tb3/i33LciGZ02n+tHivmlu4ViSTshL8'
+    '$argon2id$v=19$m=15000,t=2,p=1$WP6DUOYysF/UMi/ZiWbFIQ$HRpXoDHj9THWJa/E/bLIcJchye0jyXb2mz6P1olR1qg'
 );
