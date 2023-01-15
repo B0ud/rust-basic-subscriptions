30ef733817eacee2ea782b679d7a3ceaef8259f8
B0ud <mehdi.boudart@gmail.com>
2023-01-15 12:08:21 +0100

    Change Post admin newsletter to set up queue

---
+++ migrations/20230115101337_create_newsletter_issues_table.sql
@@ -0,0 +1,10 @@
+-- 20230115101337_create_newsletter_issues_table.sql
+-- migrations/20220211080603_create_newsletter_issues_table.sql
+CREATE TABLE newsletter_issues (
+    newsletter_issue_id uuid NOT NULL,
+    title TEXT NOT NULL,
+    text_content TEXT NOT NULL,
+    html_content TEXT NOT NULL,
+    published_at TEXT NOT NULL,
+    PRIMARY KEY(newsletter_issue_id)
+)
